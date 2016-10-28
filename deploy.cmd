7ce29027d39327ecbada7133f7b3c4af028c0812
Stefan Gordon <stefan@stefangordon.com>
2016-10-28 09:12:24 -0700

    add moment, add echos

--- deploy.cmd
+++ deploy.cmd
@@ -63,7 +63,7 @@ IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
     SET /p NODE_EXE=<"%DEPLOYMENT_TEMP%\__nodeVersion.tmp"
     IF !ERRORLEVEL! NEQ 0 goto error
   )
-  
+
   IF EXIST "%DEPLOYMENT_TEMP%\__npmVersion.tmp" (
     SET /p NPM_JS_PATH=<"%DEPLOYMENT_TEMP%\__npmVersion.tmp"
     IF !ERRORLEVEL! NEQ 0 goto error
@@ -98,9 +98,11 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 call :SelectNodeVersion
 
 :: 3. Install Yarn
+echo Verifying Yarn Install.
 call :ExecuteCmd !NPM_CMD! install yarn -g
 
 :: 4. Install Yarn packages
+echo Installing Yarn Packages.
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd yarn install --production
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var http = require('http')
+var moment = require('moment');
+
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  // Write something that leverages a yarn installed package.
+  res.end('<html><body><p>Hello World</p><p>' + moment('gibberish').format('YYYY MM DD') + '</body></html>\n');
 }).listen(port);
